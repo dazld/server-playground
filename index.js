f0c251ba39080c37d728e6dbbccd75f218734270
Dan Peddle <dan.peddle@gmail.com>
2014-02-22 19:46:44 +0100

    log heap usage

--- index.js
+++ index.js
@@ -1,27 +1,34 @@
 var net = require('net');
-
+var maxHeap = 0;
+var maxRss = 0;
 function doConnect(done){
 	
 	var client = net.connect({
 		port: 3456
 	}, function() { //'connect' listener
-		console.log('client connected');
-		client.write('ready:\r\n');
+		// console.log('client connected');
+		// client.write('ready:\r\n');
 	});
 
 	client.on('data', function(data) {
-		console.log('rx: ',data.toString());
-		client.end();
+		// console.log('rx: ',data.toString());
+		// client.end();
 	});
 
 	client.on('error', function(err) {
-		console.log('client error', err);
+		// console.log('client error', err);
 		// done(doConnect);
 	});
 
 	client.on('end', function() {
-		console.log('client disconnected');
-		done(doConnect);
+		// console.log('client disconnected');
+		var usage = process.memoryUsage();
+		if (usage.heapUsed > maxHeap) {
+			maxHeap = usage.heapUsed;
+			console.log(maxHeap);
+		};
+		var cb = done.bind(null, doConnect);
+		process.nextTick(cb);
 	});
 
 
